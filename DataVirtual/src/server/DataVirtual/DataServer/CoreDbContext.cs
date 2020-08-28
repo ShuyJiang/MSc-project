8a92695d5e1292b982391d197fb8b87840871dac
ShuyJiang <70298031+ShuyJiang@users.noreply.github.com>
2020-08-28 02:11:12 +0100

    Add files via upload

---
+++ DataVirtual/src/server/DataVirtual/DataServer/CoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DataServer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataServer
+{
+    public class CoreDbContext : DbContext
+    {
+        public virtual DbSet<Chemical> Chemical { get; set; } //Create an entity class and add it to the Context
+        public virtual DbSet<Study> Study { set; get; }
+        public virtual DbSet<Observation> Observation { set; get; }
+        public virtual DbSet<DoseResp> DoseResp { set; get; }
+
+        public CoreDbContext(DbContextOptions<CoreDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ DataVirtual/src/server/DataVirtual/DataServer/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DataServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Connect to mysql database and add database context
+            services.AddDbContext<CoreDbContext>(options =>
+                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            services.AddCors(options => {
+                options.AddPolicy("AllowAllDomain", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin().AllowCredentials();
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowAllDomain");
+
+            app.UseMvc();
+        }
+    }
+}
